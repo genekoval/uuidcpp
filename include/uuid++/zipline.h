fff7129524ef330367625525e277d7c1aaec39e7
Gene Koval <genyaremchuk@protonmail.com>
2022-12-05 21:29:31 -0500

    Update zipline coder structure

--- include/uuid++/zipline.h
+++ include/uuid++/zipline.h
@@ -4,26 +4,29 @@
 #include <zipline/zipline>
 
 namespace zipline {
-    template <typename Socket>
-    struct coder<Socket, UUID::uuid> {
-        static auto decode(Socket& socket) -> ext::task<UUID::uuid> {
+    template <io::reader Reader>
+    struct decoder<UUID::uuid, Reader> {
+        static auto decode(Reader& reader) -> ext::task<UUID::uuid> {
             auto bytes = std::array<unsigned char, UUID::size>();
-            co_await socket.read(bytes.data(), bytes.size());
+            co_await reader.read(bytes.data(), bytes.size());
 
             const auto uuid = UUID::uuid(bytes);
             TIMBER_TRACE("decode uuid: {}", uuid);
 
             co_return uuid;
         }
+    };
 
+    template <io::writer Writer>
+    struct encoder<UUID::uuid, Writer> {
         static auto encode(
-            Socket& socket,
-            const UUID::uuid& uuid
+            const UUID::uuid& uuid,
+            Writer& writer
         ) -> ext::task<> {
             TIMBER_TRACE("encode uuid: {}", uuid);
 
             const auto bytes = uuid.bytes();
-            co_await socket.write(bytes.data(), bytes.size());
+            co_await writer.write(bytes.data(), bytes.size());
         }
     };
 }
